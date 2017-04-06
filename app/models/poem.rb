ba43887f8692440f6de1fd290b8de71f1196ac46
shuji otsuka <eng.trinity.24.so.6263@gmail.com>
2017-04-06 13:51:27 +0900

    add her gem

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,3 +1,3 @@
-class Poem < ActiveResource::Base
-  self.site = "https://desolate-meadow-34788.herokuapp.com"
+class Poem < ActiveRecord::Base
+#  self.site = "https://desolate-meadow-34788.herokuapp.com"
 end
---
+++ config/initializers/her.rb
@@ -0,0 +1,15 @@
+# config/initializers/her.rb
+Her::API.setup url: "https://desolate-meadow-34788.herokuapp.com" do |c|
+  # Request
+  c.use Faraday::Request::UrlEncoded
+
+  # Response
+  c.use Her::Middleware::DefaultParseJSON
+
+  # Adapter
+  c.use Faraday::Adapter::NetHttp
+end
+
+class Poem
+  include Her::Model
+end
