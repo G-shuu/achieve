63483bacf3df352a8d39e2fe3fb52c43df6d8148
Your Name <you@example.com>
2017-05-10 13:57:02 +0900

    pusher comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,16 @@ class Comment < ActiveRecord::Base
 
   has_many :notifications, dependent: :destroy
 
+  def comment_function
+    Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
+      message: 'あなたの作成したブログにコメントが付きました'
+    })
+  end
+
+#  def comment_notification
+#  Pusher.trigger("user_#{@comment.blog.user_id}_channel", notification_created, {
+#    unread_counts: Notification.where(user_id: @comment.blog.user_id, read: false).count
+#  })
+#  end
+
 end
