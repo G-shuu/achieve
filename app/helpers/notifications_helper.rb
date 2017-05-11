07539b16e2b3e4a91362fab91e81d47f88ad6ca2
Your Name <you@example.com>
2017-05-11 21:44:38 +0900

    notification amend_5

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,8 @@ module NotificationsHelper
   def posted_time(time)
     time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
   end
+
+  def more_than
+    @notifications_count >= 1
+  end
 end
